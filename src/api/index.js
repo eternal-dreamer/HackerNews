ebff08a19ef79017e4553236af3780cb1dbf7134
Shrishti <shrishtigoyal2608@gmail.com>
2023-01-21 15:42:15 +0530

    added s

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://hn.algolia.com/api/v1' });
+const API = axios.create({ baseURL: 'https://hn.algolia.com/api/v1' });
 
 export const getArticles = (pageNumber) => API.get(`/search_by_date?tags=story&page=${pageNumber}&hitsPerPage=30`);
 export const getArticlesByPopularity = (pageNumber, keyword, filters) => API.get(`/search?query=${keyword}&tags=${filters.tag}&numericFilters=created_at_i>${filters.timestampX},created_at_i<${filters.timestampY}&page=${pageNumber}&hitsPerPage=30`);
